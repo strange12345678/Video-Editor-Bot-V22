61573cf83e56c5a1f16d91695f4ac0162280c503
strange12345678 <113347850+strange12345678@users.noreply.github.com>
2026-01-03 10:45:04 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -1,7 +1,7 @@
 import os
 
-API_ID = int(os.getenv("API_ID", 22582906))
-API_HASH = os.getenv("API_HASH", "e3096dde3e27c72a50e0e53d8ab23d6a")
-BOT_TOKEN = os.getenv("BOT_TOKEN", "7665103400:AAG5FTwuhFFHemo2uHLEJ0rOdkt9l6BSQQw")
-MONGO_URI = os.getenv("MONGO_URI", "mongodb+srv://t72957883:cPtNRDqL9D0TtIZj@cluster0.eayp2pz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+API_ID = int(os.getenv("API_ID", 0))
+API_HASH = os.getenv("API_HASH", "0")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "")
+MONGO_URI = os.getenv("MONGO_URI", "mongodb+srv://")
 LOG_CHANNEL = int(os.getenv("LOG_CHANNEL", 0))
